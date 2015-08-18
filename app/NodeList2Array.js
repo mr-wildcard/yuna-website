e1f9c10ee95a194e769e114b94bbe8590ad0c024
Julien <dev.vialajulien@gmail.com>
2015-08-18 23:04:42 +0200

    remove old style

--- app/NodeList2Array.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(nodeList) {
-    return [].map.call(nodeList, function(element) {
-        return element;
-    })
-}
--- scripts/text-to-span.js
+++
@@ -1,5 +0,0 @@
-var text = "yuna orsini. individu féminin et singulier.";
-text.split('').forEach(function(element, index) {
-
-    console.log("<span>"+ (element === " " ? "&nbsp;" : element) +"</span>")
-})
